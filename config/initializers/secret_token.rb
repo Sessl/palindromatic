7756e0e7e01295b01ca7c93b1668e64db7267044
Suchitra Edussuriya <sse_p@hotmail.com>
2013-08-24 14:11:15 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palindromatic::Application.config.secret_key_base = 'd3b5076da62be990543e13e5432c3ff52158b86e9912b222b8650d130c35236ac36c5d64445ddbd69b8ed36438b1baba89d4ea3e9a1817700c8d82f203f3cb83'
