4fc6c75c0f0d429e880f85347031d04f3cb52264
Suchitra Edussuriya <sse_p@hotmail.com>
2013-08-24 16:20:07 -0700

    Palindrome Check Complete

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,4 +2,23 @@ class PalindromesController < ApplicationController
 	def index
 		@palindromes=Palindrome.all
 	end
+
+	def new
+		@palindrome=Palindrome.new
+	end
+
+	def create
+		p=Palindrome.new(palindromes_params)
+        if p.save
+		 redirect_to palindromes_path
+		else
+			redirect_to new_palindrome_path
+		end
+	end
+
+	private
+
+	def palindromes_params
+		params.require(:palindrome).permit(:name)
+	end
 end
--- app/models/palindrome.rb
+++ app/models/palindrome.rb
@@ -1,2 +1,9 @@
 class Palindrome < ActiveRecord::Base
+	validate :is_palindrome
+	def is_palindrome
+		if self.name.reverse  != self.name
+			self.errors.add :name,"This is not a palindrome!"
+		end
+	end
+
 end
